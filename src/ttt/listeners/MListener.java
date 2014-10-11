e0ec6f3a78910ed8603d6f39be1ac06cf5a5725d
evanshrestha <evanshrestha@gmail.com>
2014-10-11 02:47:51 -0500

    Autoformatted code

--- src/ttt/listeners/MListener.java
+++ src/ttt/listeners/MListener.java
@@ -5,36 +5,36 @@ import java.awt.event.MouseListener;
 
 import ttt.gui.Frame;
 
-public class MListener implements MouseListener{
+public class MListener implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		Frame.onClick(e.getX(),e.getY());
+		Frame.onClick(e.getX(), e.getY());
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
