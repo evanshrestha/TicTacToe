2b2a8733dc5c09fc26e200cbfe42beb7667406e5
evanshrestha <evanshrestha@gmail.com>
2014-10-11 01:59:15 -0500

    Revert "Revert "Initial commit""
    This reverts commit 4443bc3c7c822e5837736e3b3a0eb2c63725e23f.

---
+++ src/ttt/gui/Frame.java
@@ -0,0 +1,19 @@
+package ttt.gui;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	public Frame(){
+		setTitle("Tic Tac Toe");
+		setSize(600,600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args){
+		
+	}
+	private static init(){
+		
+	}
+	
+}
